47ef46f2fb80e44e7d8aa48bbfad1ea73534f866
Dukeep <georgexgame2001@gmail.com>
2023-09-02 13:20:18 -0300

    Correcao do Projeto

--- Tarefa_Wrapper.java
+++ Tarefa_Wrapper.java
@@ -1,11 +1,14 @@
-public class Tarefa_Wrapper {
+public  class Tarefa_Wrapper {
+
     public static void main(String[] args) {
 
-        Integer idade = 22;
-        int i =  idade ;
-        System.out.println(i);
+       int idade = Integer.valueOf(22);
+       System.out.println(idade);
+
+
 
     }
-
-
 }
+
+
+
